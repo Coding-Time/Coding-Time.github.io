9fad61cd0d2586accf447366a2a429c1dfe7cc73
hundekoerper <el.danowar@googlemail.com>
2015-08-23 15:41:16 +0200

    add jquery magic

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
-var main = function (){};
+var main = function (){
+  $('.procontent').hide();
+
+  $('.proitem').click(function(){
+    if($(this).hasClass('selected')){
+      $(this).removeClass('selected');
+      $('.procontent').hide();
+    } else {
+      $('.proitem').removeClass('selected');
+      $('.procontent').hide();
+
+      $(this).addClass('selected');
+      $(this).next('.procontent').show();
+    }
+  });
+};
 
 $(document).ready(main);
